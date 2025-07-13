25a14b8a5dfd8b998798b05fce7bc5fa383763c0
K-Z-F-M <kimzfmagnaye@yahoo.com>
2025-07-13 11:58:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
             window.scrollTo({ top: 0, behavior: 'smooth' });
         });
     }
+
+document.addEventListener('DOMContentLoaded', function () {
+        const toggle = document.querySelector('.mobile-toggle');
+        const menu = document.querySelector('.mobile-menu');
+
+        toggle.addEventListener('click', function () {
+            menu.classList.toggle('show');
+        });
+    });
     
     const borderSlider = document.getElementById("borderSlider");
     if (borderSlider) {
